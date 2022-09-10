b67fa36c668668d77f412d01e23aabdf843327a3
Rabia <rabia.nur.tunen@ogrenci.karatay.edu.tr>
2022-09-10 15:03:01 +0300

    .

---
+++ lib/model/user_model.dart
@@ -0,0 +1,30 @@
+class UserModel {
+  String? username;
+  String? email;
+  String? name;
+  String? surname;
+  String? password;
+
+  UserModel(
+      {this.username, this.email, this.name, this.surname, this.password});
+
+  factory UserModel.fromMap(map) {
+    return UserModel(
+      username: map['username'],
+      email: map['email'],
+      name: map['name'],
+      surname: map['surname'],
+      password: map['passwod'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'username': username,
+      'email': email,
+      'name': name,
+      'surname': surname,
+      'password': password,
+    };
+  }
+}
