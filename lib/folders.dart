6a2a2f6e13d459f5085fd5a752dcb0b1f2018cfb
Rabia <rabia.nur.tunen@ogrenci.karatay.edu.tr>
2022-09-28 12:20:11 +0300

    .

---
+++ lib/folders.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Folders extends StatefulWidget {
+  const Folders({Key? key}) : super(key: key);
+
+  @override
+  State<Folders> createState() => _FoldersState();
+}
+
+class _FoldersState extends State<Folders> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text(
+          '44444444444'
+        ),
+      ),
+    );
+  }
+}
