ec188e6c5534ef5adc591210d42824b0dab5e8de
Rabia <rabia.nur.tunen@ogrenci.karatay.edu.tr>
2022-09-28 12:18:32 +0300

    .

---
+++ lib/settings.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text(
+          '555555555555'
+        ),
+      ),
+    );
+  }
+}
