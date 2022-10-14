6c7b26dc5acfebda173571d8109f9b11ed1c6f9e
Rabia <rabia.nur.tunen@ogrenci.karatay.edu.tr>
2022-10-14 15:08:14 +0300

    init

--- lib/settings.dart
+++ lib/settings.dart
@@ -13,9 +13,7 @@ class _SettingsState extends State<Settings> {
     return const Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Text(
-          ''
-        ),
+        child: Text(''),
       ),
     );
   }
